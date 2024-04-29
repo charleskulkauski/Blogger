812ba790ad901ef0d44ec45ba3912cd123d47147
Charles Kulkauski <charles.kulkauski@sptech.school>
2024-04-29 20:32:13 -0300

    Generating Jar file

--- managment/managment/src/main/java/com/incorp/managment/repository/AdminRepository.java
+++ managment/managment/src/main/java/com/incorp/managment/repository/AdminRepository.java
@@ -3,10 +3,12 @@ package com.incorp.managment.repository;
 import com.incorp.managment.model.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface AdminRepository extends JpaRepository<Admin, Long> {
 
     List<Admin> findAll();
--- managment/managment/src/main/java/com/incorp/managment/repository/MessageRepository.java
+++ managment/managment/src/main/java/com/incorp/managment/repository/MessageRepository.java
@@ -2,6 +2,8 @@ package com.incorp.managment.repository;
 
 import com.incorp.managment.model.MessageOfVisitor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MessageRepository extends JpaRepository<MessageOfVisitor, Long>{
 }
